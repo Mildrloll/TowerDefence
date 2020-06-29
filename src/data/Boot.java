95b1080d1750123e51a68b7815bd9a4b29b8b67f
Mildrloll <42993951+Mildrloll@users.noreply.github.com>
2020-06-29 15:10:37 +0200

    Made a drawQuad method that makes a box with the giving size and place.

--- src/data/Boot.java
+++ src/data/Boot.java
@@ -3,26 +3,15 @@ package data;
 import org.lwjgl.opengl.Display;
 
 import static helpers.Artist.beginSession;
+import static helpers.Artist.drawQuad;
 import static org.lwjgl.opengl.GL11.*;
 
 public class Boot {
     public Boot() {
         beginSession();
-        float width = 50;
-        float height = 50;
-        float x = 100;
-        float y = 100;
         while (!Display.isCloseRequested()) {
-            glBegin(GL_LINES);
-            glVertex2f(10, 10);
-            glVertex2f(100, 100);
-            glEnd();
-            glBegin(GL_QUADS);
-            glVertex2f(x, y); //Top left corner
-            glVertex2f(x + width, y); //Top right corner
-            glVertex2f(x + width, y + height); //Bottom right corner
-            glVertex2f(x, y + height); //Bottom Left corner
-            glEnd();
+            drawQuad(50, 50, 100, 100);
+            drawQuad(150, 150, 100, 100);
             Display.update();
             Display.sync(60);
         }
