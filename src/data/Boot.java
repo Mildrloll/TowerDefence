3af0feacdae8ec93f98bbd556bb6f15772ce9cb7
Mildrloll <42993951+Mildrloll@users.noreply.github.com>
2020-06-29 13:26:00 +0200

    Added a window

---
+++ src/data/Boot.java
@@ -0,0 +1,21 @@
+package data;
+
+import org.lwjgl.LWJGLException;
+import org.lwjgl.opengl.Display;
+import org.lwjgl.opengl.DisplayMode;
+
+public class Boot {
+    public Boot() {
+        Display.setTitle("Tower Defence");
+        try {
+            Display.setDisplayMode(new DisplayMode(600, 400));
+            Display.create();
+        } catch (LWJGLException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        new Boot();
+    }
+}
