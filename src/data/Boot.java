9779790b3b7588cf6ead7a6545b6b5f9c3841226
Mildrloll <42993951+Mildrloll@users.noreply.github.com>
2020-06-29 14:51:06 +0200

    Made a box in the window

--- src/data/Boot.java
+++ src/data/Boot.java
@@ -18,10 +18,21 @@ public class Boot {
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
         glOrtho(0, 600, 400, 0, 1, -1);
+        glMatrixMode(GL_MODELVIEW);
+        float width = 50;
+        float height = 50;
+        float x = 100;
+        float y = 100;
         while (!Display.isCloseRequested()) {
             glBegin(GL_LINES);
             glVertex2f(10, 10);
             glVertex2f(100, 100);
+            glEnd();
+            glBegin(GL_QUADS);
+            glVertex2f(x, y); //Top left corner
+            glVertex2f(x + width, y); //Top right corner
+            glVertex2f(x + width, y + height); //Bottom right corner
+            glVertex2f(x, y + height); //Bottom Left corner
             glEnd();
             Display.update();
             Display.sync(60);
