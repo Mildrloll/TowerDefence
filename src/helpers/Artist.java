998ced85d5cd6465c06f42f68ba21e92a67bafdb
Mildrloll <42993951+Mildrloll@users.noreply.github.com>
2020-07-01 13:23:42 +0200

    Made a method in the Artist class that makes tiles with texture

--- src/helpers/Artist.java
+++ src/helpers/Artist.java
@@ -3,25 +3,27 @@ package helpers;
 import org.lwjgl.LWJGLException;
 import org.lwjgl.opengl.Display;
 import org.lwjgl.opengl.DisplayMode;
+import org.newdawn.slick.opengl.Texture;
 
 import static org.lwjgl.opengl.GL11.*;
 import static org.lwjgl.opengl.GL11.GL_MODELVIEW;
 
 public class Artist {
-    public static final int WIDTH = 600, HEIGHT = 400;
+    public static final int WIDTH = 1280, HEIGHT = 960;
 
     public static void beginSession() {
         Display.setTitle("Tower Defence");
         try {
-            Display.setDisplayMode(new DisplayMode(600, 400));
+            Display.setDisplayMode(new DisplayMode(WIDTH, HEIGHT));
             Display.create();
         } catch (LWJGLException e) {
             e.printStackTrace();
         }
         glMatrixMode(GL_PROJECTION);
         glLoadIdentity();
-        glOrtho(0, 600, 400, 0, 1, -1);
+        glOrtho(0, WIDTH, HEIGHT, 0, 1, -1);
         glMatrixMode(GL_MODELVIEW);
+        glEnable(GL_TEXTURE_2D);
     }
 
     public static void drawQuad(float x, float y, float width, float height) {
@@ -30,6 +32,22 @@ public class Artist {
         glVertex2f(x + width, y); //Top right corner
         glVertex2f(x + width, y + height); //Bottom right corner
         glVertex2f(x, y + height); //Bottom Left corner
+        glEnd();
+    }
+
+    public static void drawQuadTex(Texture tex, float x, float y, float width, float height) {
+        tex.bind();
+        glTranslatef(x, y, 0);
+        glBegin(GL_QUADS);
+        glTexCoord2f(0, 0);
+        glVertex2f(0, 0);
+        glTexCoord2f(1, 0);
+        glVertex2f(width, 0);
+        glTexCoord2f(1, 1);
+        glVertex2f(width, height);
+        glTexCoord2f(0, 1);
+        glVertex2f(0, height);
+        glLoadIdentity();
         glEnd();
     }
 }
